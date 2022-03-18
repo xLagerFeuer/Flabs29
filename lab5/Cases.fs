37ea05a8d7633e62f42e17ccbf39a413ce0c99cc
GeroLager <alexander.zhdanoff@gmail.com>
2022-03-18 19:34:27 +0300

    initial project VS

---
+++ lab5/Cases.fs
@@ -0,0 +1,75 @@
+﻿// open Utils
+// open System
+
+module Cases
+
+// 11.
+
+let case11caseStatement langName =
+    ()
+
+// 12. - реализация внутри main
+
+// 13.
+
+let case13prodRecursionUpwards =
+    ()
+
+let case13prodTailCall =
+    ()
+
+// 14.
+
+let case14divSearch =
+    ()
+
+// 15.
+
+let case15coprimeNumbers X func initY = // declare types defination
+    ()
+
+// 16.
+
+let case16unitTest15 =
+    ()
+
+let case16eulerNumber =
+    ()
+
+let case16gcd =
+    ()
+
+// 17.
+
+let case17divsSearch = //
+    ()
+
+// 18. Work with numbers
+// Variant 3 — Zhdanoff Alexader
+
+// 1
+
+let maxgcd =
+    ()
+
+let case181 =
+    ()
+
+// 2
+
+let prodNonDiv5 = // TODO: ???
+    ()
+
+let case182 =
+    ()
+
+// 3
+
+let gcd =
+    ()
+
+let maxEvenNonPrime = // TODO: делитель числа, его произведения
+    ()
+
+let case183 =
+    ()
---
+++ lab5/Main.fs
@@ -0,0 +1,26 @@
+﻿module Main
+
+open Cases
+
+[<EntryPoint>]
+let main argv =
+    case11caseStatement "Python"
+    
+    // case12CaseStatement - first superpositions then carring
+    
+        // гег
+
+    //
+
+    // case13prodRecursionUpwards
+    // case13prodTailCall
+    // case14divSearch
+    // case15coprimeNumbers
+    // case16eulerNumber
+    // case16gcd
+    // case17divsSearch
+    // case181
+    // case182
+    // case183
+
+    0 // return 0
