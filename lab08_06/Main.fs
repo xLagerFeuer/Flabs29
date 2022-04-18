8423f3af56474a62fcb68ea869743316a3cbfeae
xLagerFeuer <alexander.zhdanoff@gmail.com>
2022-04-18 14:37:23 +0300

    lab08_06

---
+++ lab08_06/Main.fs
@@ -0,0 +1,20 @@
+﻿open FParsec
+open System
+ 
+let MailBoxAgent = MailboxProcessor.Start(
+    fun inbox ->
+        let rec messageLoop() = async{
+            let! msg = inbox.Receive()
+            if msg % 15 = 0 then Console.WriteLine($"{msg} - YES")
+            else Console.WriteLine($"{msg} - NO")
+            return! messageLoop()
+        }
+        messageLoop()
+    )
+
+
+[<EntryPoint>]
+let main argv =
+    for i in [1..150] do
+        MailBoxAgent.Post(i)
+    0 
