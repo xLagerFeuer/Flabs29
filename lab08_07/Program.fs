9bc7986eae99408f27f87abd8be6295b26b59783
xLagerFeuer <alexander.zhdanoff@gmail.com>
2022-04-18 16:32:19 +0300

    lab08_07

---
+++ lab08_07/Program.fs
@@ -0,0 +1,49 @@
+﻿open FParsec
+open System
+
+type Expr =
+    | Num of float
+    | Plus of Expr * Expr
+    | Minus of Expr * Expr
+
+let pstring_trim s = spaces >>. pstring s .>> spaces
+let float_ws = spaces >>. pfloat .>> spaces
+
+
+let parseExpression, implementation = createParserForwardedToRef<Expr, unit>()
+
+let parsePlus = tuple2 (parseExpression .>> pstring_trim "+") parseExpression |>> Plus
+let parseSub = tuple2 (parseExpression .>> pstring_trim "-") parseExpression |>> Minus
+
+let parseOp = between (pstring_trim "(") (pstring_trim ")") (attempt parsePlus <|> parseSub)
+
+let parseNum = float_ws |>> Num
+
+implementation := parseNum <|> parseOp
+
+let rec EvalExpr (e:Expr): float =
+    match e with
+    | Num(num) -> num
+    | Plus(a,b) ->
+        let left = EvalExpr(a)
+        let right = EvalExpr(b)
+        let result = left + right
+        result
+    | Minus(a,b) ->
+        let left = EvalExpr(a)
+        let right = EvalExpr(b)
+        let result = left - right
+        result
+
+
+
+[<EntryPoint>]
+let main argv =
+    let input = Console.ReadLine() 
+    let expr = run parseExpression input
+
+    match expr with
+    | Success (result, _, _) -> Console.WriteLine(EvalExpr result)
+    | Failure (errorMsg, _, _) -> Console.WriteLine(errorMsg)
+
+    0
